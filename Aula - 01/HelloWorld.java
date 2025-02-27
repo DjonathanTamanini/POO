f3aa013a6352118a0323a46c698b2d0b63d05d6f
Djonathan Tamanini <djonathan.tamanini@gmail.com>
2025-02-26 21:56:28 -0300

    Fim da aula

---
+++ Aula - 01/HelloWorld.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HelloWorld{
+
+// Método inicial que inicia o programa
+
+	public static void main(String args[]){
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Digite um número: ");
+		int i = leitor.nextInt();
+		System.out.println("Você digitou: " +i);
+//Compilar usando #javac classe.java
+//Rodar usando #java classe
+
+}
+
+}
